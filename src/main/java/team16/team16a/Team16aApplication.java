5d50fae491692d145028512ddc63873d5bbc01d3
e1b19099 <e1b19099@oit.ac.jp>
2021-10-05 14:52:38 +0900

    リポジトリの初期化

---
+++ src/main/java/team16/team16a/Team16aApplication.java
@@ -0,0 +1,13 @@
+package team16.team16a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team16aApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Team16aApplication.class, args);
+  }
+
+}
---
+++ src/test/java/team16/team16a/Team16aApplicationTests.java
@@ -0,0 +1,13 @@
+package team16.team16a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team16aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
